57b5a37ac0538e8735a95cfebb13d994d1b1aa33
Abel-Git2103 <abel_jornet@hotmail.com>
2022-03-14 10:07:58 +0100

    DADO1

--- src/app/dado/dado.component.ts
+++ src/app/dado/dado.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DadoComponent implements OnInit {
 
+    valor: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.valor = Math.trunc(Math.random() * 6) + 1;
   }
 
 }
